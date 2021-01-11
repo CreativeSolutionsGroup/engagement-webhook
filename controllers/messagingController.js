a662959e59e0ab93798ed69214e90165b0edd68e
Jake Allinson <jakeallinson03@gmail.com>
2021-01-11 17:26:42 -0500

    changes function to promises, adds error response to user

--- controllers/messagingController.js
+++ controllers/messagingController.js
@@ -8,44 +8,57 @@
 //
 
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
 const axios = require('axios');
 
 exports.handleRequest = function (req, res) {
   // new messaging object
   const twiml = new MessagingResponse();
   const message = twiml.message();
-  getEngagements(function(engagements) {
-    if (engagements && engagements.length) {
-      // check if incoming message matches (using regex) any engagement keyword
-      const matches = engagements.filter(eng => RegExp(eng.keyword).test(req.body.Body));
-      if (matches && matches.length) {
-        message.body(matches[0].message);
-        if (matches[0].image_url) {
-          message.media(matches[0].image_url);
+  getEngagements()
+    .then((engagements) => {
+      if (engagements && engagements.length) {
+        // check if incoming message matches (using regex) any engagement keyword
+        const matches = engagements.filter(eng => RegExp(eng.keyword).test(req.body.Body));
+        if (matches && matches.length) {
+          message.body(matches[0].message);
+          // check for valid image url
+          if (matches[0].image_url && matches[0].image_url != "") {
+            message.media(matches[0].image_url);
+          }
+          // send engagee to smart events
+          createNewEngagee(req.body.From, req.body.Body, matches[0]);
+        } else {
+          message.body("Hmm. I don't recognize that. Try a different message!");
         }
-        // send engagee to smart events
-        createNewEngagee(req.body.From, req.body.Body, matches[0]);
       } else {
-        message.body("Hmm. I don't recognize that.");
+        message.body("Bummer. No events seem to be happening at the moment. Check back later!");
       }
-    } else {
-      message.body("Bummer. No events seem to be happening at the moment. Check back later!");
-    }
-    // send the response
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-  });
+      // send the response
+      res.writeHead(200, {'Content-Type': 'text/xml'});
+      res.end(twiml.toString());
+    })
+    .catch(() => {
+      // send error response
+      message.body("Oops. There has been an error, try again later!");
+      res.writeHead(200, {'Content-Type': 'text/xml'});
+      res.end(twiml.toString());
+    })
 };
 
 const apiRoute = "http://18.222.7.110"
 
-function getEngagements(callback) {
-  axios.get(`${apiRoute}/api/engagements`)
-    .then((response) => {
-      callback(response.data.data);
-    })
-    .catch((error) => { console.log(error) });
+function getEngagements()
+{
+  return new Promise((resolve, reject) => {
+    axios.get(`${apiRoute}/api/engagements`)
+      .then((response) => {
+        resolve(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        reject();
+      })
+ });
 }
 
 function createNewEngagee(phone, message, engagement) {
