73fa2bba1c424f5aa279450048a60e42961496f1
spbills <spencebills0@gmail.com>
2021-09-24 01:17:10 +0000

    Added cronjob parsing 30 minutes earlier.

--- helpers/slots.js
+++ helpers/slots.js
@@ -2,13 +2,23 @@
 // Runs every 30 seconds and checks.
 
 import cron from "cron";
+import Axios from "axios";
 
-const checkSlots = async () => {
-    const job = new cron.CronJob("*/30 * * * * *", () => {
-        console.log("done");
-    });
+const MS_PER_MINUTE = 60000;
 
-    job.start();
+const registerSlotManager = async () => {
+    const slotResponse = await Axios.get("https://api.cusmartevents.com/api/slots");
+    const dates = slotResponse.data.data;
+
+    for (let date of dates) {
+        const thirtyEarlier = new Date(Date.parse(date.hide_time) - 30 * MS_PER_MINUTE);
+        const job = new cron.CronJob(thirtyEarlier, () => {
+            console.log("done");
+        });
+
+        job.start();
+    }
+    
 }
 
-export default checkSlots
+export default registerSlotManager;
