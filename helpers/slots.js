ced107262edbf4b143858489ecacf595ce955303
spbills <spencebills0@gmail.com>
2021-09-24 01:25:27 +0000

    Added time checking.

--- helpers/slots.js
+++ helpers/slots.js
@@ -7,18 +7,19 @@ import Axios from "axios";
 const MS_PER_MINUTE = 60000;
 
 const registerSlotManager = async () => {
-    const slotResponse = await Axios.get("https://api.cusmartevents.com/api/slots");
+    const slotResponse = await Axios.get(`${process.env.API_URL}/api/slots`);
     const dates = slotResponse.data.data;
 
     for (let date of dates) {
         const thirtyEarlier = new Date(Date.parse(date.hide_time) - 30 * MS_PER_MINUTE);
-        const job = new cron.CronJob(thirtyEarlier, () => {
-            console.log("done");
-        });
+        if (thirtyEarlier.getMilliseconds() > Date.now()) {
+            const job = new cron.CronJob(thirtyEarlier, () => {
+                console.log("done");
+            });
 
-        job.start();
+            job.start();
+        }
     }
-    
 }
 
 export default registerSlotManager;
